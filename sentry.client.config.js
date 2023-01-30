33b688c90c46273c823c394048f4dd8f2d04e45a
Max <madstk1@pm.me>
2023-01-30 13:33:32 +0100

    chore: remove comments fom sentry configs

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -8,10 +8,5 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN || 'https://534eb0061cdb4855ad195eef6b624733@o4504589833011200.ingest.sentry.io/4504589860470784',
-  // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
 });
