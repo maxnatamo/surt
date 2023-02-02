7ed462dba12cccc5a4501eb49f1bbf8860f40434
Max <madstk1@pm.me>
2023-02-02 19:53:08 +0100

    feat: add support for path segments

--- libraries/url.js
+++ libraries/url.js
@@ -5,7 +5,7 @@
  * @returns True, if the URL is valid and can be shortened. Otherwise, false.
  */
 export const verify = (target) => {
-  const regex = /^(http|https):\/\/(www\.)?([^ "]+)\.[a-z]+$/;
+  const regex = /^(http|https):\/\/(www\.)?([^ "]+)\.[a-z]+[A-Za-z0-9-_~:/?#\[\]@!$&'()*+,;%=.]*$/;
 
   return regex.test(target);
 }
--- libraries/url.test.js
+++ libraries/url.test.js
@@ -1,58 +1,70 @@
 import { verify } from './url';
 
 describe('Verify URLs', () => {
-  it('expect hostname without protocol to return false', () => {
+  it('expect URL without protocol to return false', () => {
     const url = "example.com";
 
     expect(verify(url)).toBe(false);
   });
 
-  it('expect hostname without TLD to return false', () => {
+  it('expect URL without TLD to return false', () => {
     const url = "example";
 
     expect(verify(url)).toBe(false);
   });
 
-  it('expect hostname with HTTP scheme to return true', () => {
+  it('expect URL with HTTP scheme to return true', () => {
     const url = "http://example.com";
 
     expect(verify(url)).toBe(true);
   });
 
-  it('expect hostname with HTTP scheme and without protocol to return false', () => {
+  it('expect URL with HTTP scheme and without protocol to return false', () => {
     const url = "http://example";
 
     expect(verify(url)).toBe(false);
   });
 
-  it('expect hostname with HTTP scheme to return true', () => {
+  it('expect URL with HTTP scheme to return true', () => {
     const url = "https://example.com";
 
     expect(verify(url)).toBe(true);
   });
 
-  it('expect hostname with FTP scheme to return false', () => {
+  it('expect URL with FTP scheme to return false', () => {
     const url = "ftp://example.com";
 
     expect(verify(url)).toBe(false);
   });
 
-  it('expect hostname with file scheme to return false', () => {
+  it('expect URL with file scheme to return false', () => {
     const url = "file://example.com";
 
     expect(verify(url)).toBe(false);
   });
 
-  it('expect hostname with file scheme to return false', () => {
+  it('expect URL with www and TLD to return true', () => {
     const url = "http://www.example.com";
 
     expect(verify(url)).toBe(true);
   });
 
   // This should honestly return false, but the regex would be insane with TLDs.
-  it('expect hostname with file scheme to return true', () => {
+  it('expect URL with file scheme to return true', () => {
     const url = "http://www.example";
 
     expect(verify(url)).toBe(true);
+  });
+
+  it('expect URL with all valid supported characters in path segment to return true', () => {
+    const url = "http://www.example.com/ASDasd0123456789-_.~:/?#[]@!$&'()*.+,;%=";
+
+    expect(verify(url)).toBe(true);
+  });
+
+  it('expect URL with a single invalid character in path segment to return false', () => {
+    const url = "http://www.example.com/<";
+
+    expect(verify(url)).toBe(false);
   });
 });
