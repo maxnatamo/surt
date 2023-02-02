2ca92981baad6a6fccf1f59f60d3c4b45da33c4a
Max <madstk1@pm.me>
2023-02-02 23:03:52 +0100

    fix: add lang attribute to html-tag for Lighthouse score

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <meta name="description" content="Surt - a simple URL shortener" />
         <link rel="icon" href="/favicon.ico" />
